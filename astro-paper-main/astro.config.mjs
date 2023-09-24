aa5b4dec5adb1589a8cb54557bb9494b2d911982
a.miens <andrea-isaac.miens@telecom-sudparis.eu>
2023-09-24 22:35:29 +0200

    Template

--- astro-paper-main/astro.config.mjs
+++ astro-paper-main/astro.config.mjs
@@ -5,6 +5,11 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://andreamiens.github.io',
+})
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
